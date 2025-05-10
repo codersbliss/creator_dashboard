ad8500a2d92f2eac19b1c0c1c8461107cde0632b
coderbliss <prateek.2022ca068@mnnit.ac.in>
2025-05-10 12:22:09 +0530

    update

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://dashboard-ui-7awv.onrender.com',
+    credentials: true,  // if you’re using cookies or Authorization headers
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
